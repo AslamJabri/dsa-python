16263716b7f30e5a37d6413bdce3359c06c1e8db
aslamjabri <jabri.aslam@outlook.com>
2026-01-12 18:25:46 +0530

    Started Array

--- array/array.py
+++ array/array.py
@@ -10,4 +10,25 @@ added_sum =0
 for i in range(len(sum)):
     
     added_sum += sum[i] 
-print(added_sum)
+print(f"Sum of all numbers: {added_sum}")
+
+
+# Find Maximum Element
+elements = [3, 7, 2, 9, 1]
+highest_element = elements[0]
+for element in elements:
+    if element > highest_element:
+        highest_element = element
+    
+print(f"Highest element : {highest_element}")
+
+#Find Minimum Element
+
+numbers = [8, 4, 6, 2]
+minimum = numbers[0]
+
+for num in numbers:
+    if num < minimum:
+        minimum = num
+print(f"Minimum Number: {minimum}")
+
